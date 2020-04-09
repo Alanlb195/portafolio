5b82b697c77b4858f1de211047e9ee7aeb985a91
alanlb <alanlb195@gmail.com>
2020-04-09 16:32:28 -0500

    info page, to footer and header

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPage } from '../interfaces/info-page';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPageService {
 
-  info: any = { };
+  info: InfoPage = { };
   cargada = false;
 
   constructor(private httpClient: HttpClient) {
 
     // Cargar datos del json data-page
     this.httpClient.get('assets/data/data-page.json')
-      .subscribe( resp => {
+      .subscribe( (resp: InfoPage ) => {
 
         this.cargada = true;
         this.info = resp;
