58454bdca97b1014e4a8127f1e42a36180e3b20d
alanlb <alanlb195@gmail.com>
2020-04-12 11:47:06 -0500

    productos - portafolio page

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Productos } from '../interfaces/productos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: Productos [] = [];
+
+  constructor( public http: HttpClient ) {
+
+    this.cargarProductos();
+  }
+
+
+  cargarProductos() {
+
+    this.http.get('https://angular-template-1ac59.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: Productos[]) => {
+
+      console.log(resp);
+      this.productos = resp;
+
+      setTimeout(() => {
+
+        this.cargando = false;
+      }, 2000);
+
+    });
+  }
+
+}
