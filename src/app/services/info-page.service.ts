0f9e3a7fb6ed1c344116ae7b0ae336ee0cd2f28c
alanlb <alanlb195@gmail.com>
2020-04-12 02:43:18 -0500

    firebase about people data

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -7,21 +7,38 @@ import { InfoPage } from '../interfaces/info-page';
 })
 export class InfoPageService {
 
-  info: InfoPage = { };
+  info: InfoPage = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private httpClient: HttpClient) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+
     // Cargar datos del json data-page
     this.httpClient.get('assets/data/data-page.json')
-      .subscribe( (resp: InfoPage ) => {
+      .subscribe((resp: InfoPage) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log( resp );
-
       });
-
   }
+
+  private cargarEquipo() {
+
+    // Cargar datos del json data-page
+    this.httpClient.get('https://angular-template-1ac59.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+        this.equipo = resp;
+        console.log(resp);
+      });
+  }
+
+
 
 }
